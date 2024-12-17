04b8088b737852f13ef00c77662945442dd777f7
vishwa8287 <vishwa8782@gmail.com>
2024-12-17 22:41:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// JavaScript for interactive features
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('click', () => {
-        alert('You clicked on a card!');
-    });
-});
+
